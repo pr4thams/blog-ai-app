40d91c8d60c74dbc4fec36ef15407b31f531ecd2
Pratham Sharma <sharma.pratham0502@gmail.com>
2023-05-13 16:10:34 -0230

    added openai content and production env

--- app/post/[id]/Article.tsx
+++ app/post/[id]/Article.tsx
@@ -1,6 +1,7 @@
 import { Editor, EditorContent } from "@tiptap/react";
-import React from "react";
+import React, { useState } from "react";
 import EditorMenuBar from "./EditorMenuBar";
+import { RocketLaunchIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   contentError: string;
@@ -17,11 +18,53 @@ const Article = ({
   setContent,
   title,
 }: Props) => {
+  const [role, setRole] = useState<string>("I am a helpful assistant.");
+
   if (!editor) {
     return null;
   }
+
+  const postAiContent = async () => {
+    editor
+      .chain()
+      .focus()
+      .setContent("Generating AI Content. Please Wait...")
+      .run();
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/openai`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: title,
+        role: role,
+      }),
+    });
+    const data = await response.json();
+
+    editor.chain().focus().setContent(data.content).run();
+    setContent(data.content);
+  };
+
   return (
     <article className="text-wh-500 leading-8">
+      {/* AI Generator */}
+      {isEditable && (
+        <div className="border-2 rounded-md bg-wh-50 p-3 mb-3">
+          <h4 className="m-0 p-0">Generate AI Content</h4>
+          <p className="my-1 p-0 text-xs"> What type of write do you want?</p>
+          <div className="flex gap-5 justify-between">
+            <input
+              className="border-2 rounded-md bg-wh-50 px-3 py-1 w-full"
+              placeholder="Role"
+              onChange={(e) => setRole(e.target.value)}
+              value={role}
+            />
+            <button type="button" onClick={postAiContent}>
+              <RocketLaunchIcon className="h-8 w-8 text-accent-orange hover:text-wh-300" />
+            </button>
+          </div>
+        </div>
+      )}
       <div
         className={
           isEditable ? "border-2 rounded-md bg-wh-50 p-3" : "w-full max-w-full"
